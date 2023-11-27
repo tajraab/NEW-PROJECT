389ff43998b2b2680e45b9a0673a898ccff3e394
tajraab <tajrabacevac@gmail.com>
2023-11-27 18:48:08 +0100

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,14 @@ function Header() {
   return (
     <div className="headerContainer">
       <div classNaame="logo">
-        <h1>Logo</h1>
+        {/* <h1>Logo</h1> */}
+        <img src=" https://topliste.rs/wp-content/uploads/2020/05/ethereum-3818348_1920.jpg" />
       </div>
-      <div className="links">
-        <h3>All products</h3>
-        <h3>About</h3>
-        <h3>Contact us</h3>
-      </div>
+        <div className="links">
+          <h3>All products</h3>
+          <h3>About</h3>
+          <h3>Contact us</h3>
+        </div>
     </div>
   );
 }
