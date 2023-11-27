999b243e81965f7d4e3c3e04d313133e5120f484
tajraab <tajrabacevac@gmail.com>
2023-11-27 18:21:40 +0100

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
-
+import "./header.css";
 function Header() {
   return (
     <div className="headerContainer">
-        <p>Tajra</p>
+      <div classNaame="logo">
+        <h1>Logo</h1>
       </div>
+      <div className="links">
+        <h3>All products</h3>
+        <h3>About</h3>
+        <h3>Contact us</h3>
+      </div>
+    </div>
   );
 }
 
