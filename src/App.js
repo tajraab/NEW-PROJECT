4bca0d8049c4bd561db0e2233b30bc75b067c6ef
tajraab <tajrabacevac@gmail.com>
2023-11-29 17:36:46 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Header from "./components/Header/Header";
-import {useNavigate,Router}from "react-router-dom";
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NewsPage from './components/Pages/NewsPage';
+import ExchangesPage from './components/Pages/ExchangesPage';
+import Crypto from './components/Pages/Crypto';
+// import LogoPage from './components/Pages/HomePage';
+import HomePage from './components/Pages/HomePage';
+// import Card from './components/Card/Card';
 function App() {
   return (
     <div className="App">
---
+++ src/components/Pages/Crypto.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Crypto() {
+  return <div><h1>Crypto</h1></div>;
+}
+
+export default Crypto;
