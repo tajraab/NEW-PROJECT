7e8ec6b7101b401325d48ee4482794737fc9adc0
tajraab <tajrabacevac@gmail.com>
2023-11-29 17:48:00 +0100

    .

---
+++ src/components/Pages/HomePage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+import NewsPage from "./NewsPage";
+import Card from "../Card/Card";
+function HomePage() {
+  const[coinData, setCoinData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coins',
+        params: {
+          referenceCurrencyUuid: 'yhjMzLPhuIDl',
+          timePeriod: '24h',
+          'tiers[0]': '1',
+          orderBy: 'marketCap',
+          orderDirection: 'desc',
+          limit: '50',
+          offset: '0'
+        },
+        headers: {
+          'X-RapidAPI-Key': '39e1231c66msh1e9397f9d922770p1ac01djsn60c7f7bb2519',
+          'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+      };
+      try {
+        const response = await axios.request(options);
+        setCoinData(response.data.data.coins); // Postavljanje podataka pomoću setCoinData
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
