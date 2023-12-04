22fef70ebdd63a344ac8251261fb84db97474408
tajraab <tajrabacevac@gmail.com>
2023-12-04 16:24:48 +0100

    .

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function About({coin}) {
     return (
-        <div className="kartica">
+        <div className="my-card">
           <img src={coin.iconUrl}></img>
           {/* <p style={{width:'200px' , height:'100px'}}> {coin.iconUrl}</p> */}
               <div className="naslov">{coin.name}</div>
